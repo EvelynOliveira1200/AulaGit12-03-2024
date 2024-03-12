942656820bfcee31c5bdd621143e11c9658eaca0
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-03-12 08:52:12 -0300

    Mudança na linha número 8, um novo console foi adicionado

---
+++ temperatura.js
@@ -0,0 +1,8 @@
+let a = 25.80909;
+
+let fahrenheit = (a * 9/5) + 32;
+
+ 
+console.log("BOM DIA!! A temperatura no dia de hoje é de " + a.toFixed(1) + "°C e de " + fahrenheit.toFixed(1) + "°F "); 
+
+
