3f121b5f09badd5057b4bda8cedd2f6c0dfb58d3
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-03-12 09:25:35 -0300

    Console adicionado na linha número 14

--- temperatura.js
+++ temperatura.js
@@ -9,3 +9,8 @@ console.log("Amanhã será um dia ensolarado");
 
 console.log("Sexta-feira será um dia nublado");
 
+console.log("Semana de muita chuva, podendo ocorrer alagamentos");
+
+console.log("Calor acima do esperado no próximo feriado");
+
+
