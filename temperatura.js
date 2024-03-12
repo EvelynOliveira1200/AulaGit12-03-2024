070f1cbb9bcceb56d32468a4afdff69e277f5c02
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-03-12 09:13:39 -0300

    Console adicionado na linha número 12

--- temperatura.js
+++ temperatura.js
@@ -1,11 +1,11 @@
 let a = 25.80909;
 
-let fahrenheit = (a * 9/5) + 32;
+let fahrenheit = (a * 9 / 5) + 32;
 
 
 console.log("Boa Tarde!! A temperatura no dia de hoje é de " + a.toFixed(1) + "°C e de " + fahrenheit.toFixed(1) + "°F ");
 
-console.log("Hoje o dia está chuvoso, leve o guarda chuva!");
+console.log("Hoje o dia está muito calor, hidrate-se!");
 
 console.log("Amanhã será um dia ensolarado");
 
