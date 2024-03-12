8b8f8095388cf82eb3e8a245aacc009cafedb78f
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-03-12 09:30:42 -0300

    Console removido na linha 8

--- temperatura.js
+++ temperatura.js
@@ -5,8 +5,6 @@ let fahrenheit = (a * 9 / 5) + 32;
 
 console.log("Boa Tarde!! A temperatura no dia de hoje é de " + a.toFixed(1) + "°C e de " + fahrenheit.toFixed(1) + "°F ");
 
-console.log("Amanhã será um dia ensolarado");
-
 console.log("Sexta-feira será um dia nublado");
 
 console.log("Semana de muita chuva, podendo ocorrer alagamentos");
